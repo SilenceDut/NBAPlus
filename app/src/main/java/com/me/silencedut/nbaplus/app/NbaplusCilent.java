a80b9d508f425281c1794b288b7bc456f6039881
SilenceDutchman <silencedutchman@foxmail.com>
2015-12-02 18:06:23 +0800

    rename

---
+++ app/src/main/java/com/me/silencedut/nbaplus/app/NbaplusCilent.java
@@ -0,0 +1,24 @@
+package com.me.silencedut.nbaplus.app;
+
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
+/**
+ * Created by SilenceDut on 2015/11/28.
+ */
+public class NbaplusCilent {
+    final Nbaplus nbaplus ;
+    NbaplusCilent() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://nbaplus.sinaapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        nbaplus=retrofit.create(Nbaplus.class);
+
+    }
+
+    public Nbaplus getCilent() {
+        return nbaplus;
+    }
+}
