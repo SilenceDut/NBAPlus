89fd53bdc6e3d6d67c3f8c785cd44c352d26c255
SilenceDutchman <silencedutchman@foxmail.com>
2015-12-07 18:17:47 +0800

    modified news update api

--- app/src/main/java/com/me/silencedut/nbaplus/app/NbaplusAPI.java
+++ app/src/main/java/com/me/silencedut/nbaplus/app/NbaplusAPI.java
@@ -11,7 +11,7 @@ import rx.Observable;
  * Created by SilenceDut on 2015/11/28.
  */
 public interface NbaplusAPI {
-    @GET("api/v1.0/{type}/update")
+    @GET("api/v1.0/update/{type}")
     Observable<News> updateNews(@Path("type") String type);
     @GET("api/v1.0/{type}/{newsId}")
     Observable<News> loadmore(@Path("type") String type,@Path("newsId") String newsId );
