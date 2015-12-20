3aa4bf0955db184e4397aab225ef7d7924c021b7
silencedutchman <silencedutchman@foxmail.com>
2015-12-20 18:29:50 +0800

    rename file name

---
+++ app/src/main/java/com/me/silencedut/nbaplus/rxmethod/RxNews.java
@@ -0,0 +1,179 @@
+package com.me.silencedut.nbaplus.rxmethod;
+
+import com.me.silencedut.greendao.GreenNews;
+import com.me.silencedut.greendao.GreenNewsDao;
+import com.me.silencedut.nbaplus.app.App;
+import com.me.silencedut.nbaplus.app.AppService;
+import com.me.silencedut.nbaplus.data.Constant;
+import com.me.silencedut.nbaplus.event.NewsDetileEvent;
+import com.me.silencedut.nbaplus.event.NewsEvent;
+import com.me.silencedut.nbaplus.model.News;
+import com.me.silencedut.nbaplus.model.NewsDetile;
+import com.me.silencedut.nbaplus.utils.PreferenceUtils;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.greenrobot.dao.query.DeleteQuery;
+import de.greenrobot.dao.query.Query;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by SilenceDut on 2015/12/2.
+ */
+public class RxNews {
+    public static Subscription updateNews(final String newsType) {
+        Subscription subscription = AppService.getNbaplus().updateNews(newsType)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(new Action1<News>() {
+                    @Override
+                    public void call(News news) {
+                        cacheNews(news,newsType);
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<News>() {
+                    @Override
+                    public void call(News news) {
+                        AppService.getBus().post(new NewsEvent(news, Constant.GETNEWSWAY.UPDATE,newsType));
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        NewsEvent newsEvent= new NewsEvent(new News(), Constant.GETNEWSWAY.UPDATE,newsType);
+                        newsEvent.setEventResult(Constant.Result.FAIL);
+                        AppService.getBus().post(newsEvent);
+                    }
+                });
+        return subscription;
+    }
+
+    public static Subscription loadMoreNews(final String newsType,final String newsId) {
+        Subscription subscription = AppService.getNbaplus().loadMoreNews(newsType, newsId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<News>() {
+                    @Override
+                    public void call(News news) {
+                        AppService.getBus().post(new NewsEvent(news, Constant.GETNEWSWAY.LOADMORE,newsType));
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        NewsEvent newsEvent= new NewsEvent(new News(), Constant.GETNEWSWAY.LOADMORE,newsType);
+                        newsEvent.setEventResult(Constant.Result.FAIL);
+                        AppService.getBus().post(newsEvent);
+                    }
+                });
+        return subscription;
+    }
+
+    public static Subscription getNewsDetile(final String date,final String detileId) {
+        Subscription subscription = AppService.getNewsDetileApi().getNewsDetile(date,detileId+".json")
+                .subscribeOn(Schedulers.io())
+                .map(new Func1<NewsDetile, String>() {
+                    @Override
+                    public String call(NewsDetile newsDetile) {
+                        int imageCount=0;
+                        String CSS_STYLE =String.format(Constant.CSS_STYLE , PreferenceUtils.getPrefString(App.getContext(), Constant.ACTILEFONTSIZE, "18"),"#333333");
+                        String html ="<html><head>" +CSS_STYLE+ "</head><body>" +newsDetile.getContent()+"<p>（"+newsDetile.getAuthor()+"）<p></body></html>";
+                        Pattern p=Pattern.compile("(<p class=\"reader_img_box\"><img id=\"img_\\d\" class=\"reader_img\" src=\"reader_img_src\" /></p>)+");
+                        Matcher m=p.matcher(html);
+                        while (m.find()&&imageCount<newsDetile.getArticleMediaMap().size()) {
+                            if(imageCount==0) {
+                                html=html.replace(html.substring(m.start(), (m.end())),"");
+                            }else {
+                                if(PreferenceUtils.getPrefBoolean(App.getContext(),Constant.LOADIMAGE,true)) {
+                                    html = html.replace(html.substring(m.start(), (m.end())), "<img src="
+                                            + newsDetile.getArticleMediaMap().get(String.format("img_%d", imageCount)).getUrl() + " "
+                                            + "width=\"100%\" height=\"auto\">");
+                                }
+                            }
+                            imageCount++;
+                            m=p.matcher(html);
+                         }
+                        return html;
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String newsContent) {
+                        AppService.getBus().post(new NewsDetileEvent(newsContent));
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        NewsDetileEvent newsDetileEvent = new NewsDetileEvent(throwable.toString());
+                        newsDetileEvent.setEventResult(Constant.Result.FAIL);
+                        AppService.getBus().post(newsDetileEvent);
+                    }
+                });
+        return subscription;
+    }
+
+    public static Subscription initNews(final String newsType) {
+
+        Subscription subscription = Observable.create(new Observable.OnSubscribe<News>() {
+                    @Override
+                    public void call(Subscriber<? super News> subscriber) {
+                        News news = getCacheNews(newsType);
+                        subscriber.onNext(news);
+                        subscriber.onCompleted();
+                    }
+                }).subscribeOn(Schedulers.io())
+                .subscribe(new Action1<News>() {
+                    @Override
+                    public void call(News news) {
+                        NewsEvent newsEvent= new NewsEvent(news, Constant.GETNEWSWAY.INIT,newsType);
+                        if(news==null) {
+                            newsEvent.setEventResult(Constant.Result.FAIL);
+                        }
+                        AppService.getBus().post(newsEvent);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        NewsEvent newsEvent= new NewsEvent(new News(), Constant.GETNEWSWAY.INIT,newsType);
+                        newsEvent.setEventResult(Constant.Result.FAIL);
+                        AppService.getBus().post(newsEvent);
+                    }
+                });
+        return subscription;
+    }
+    public static void cacheNews(final News news,final String newsType) {
+
+        GreenNewsDao greenNewsDao = AppService.getDBHelper().getDaoSession().getGreenNewsDao();
+        DeleteQuery deleteQuery = greenNewsDao.queryBuilder()
+                .where(GreenNewsDao.Properties.Type.eq(newsType))
+                .buildDelete();
+        deleteQuery.executeDeleteWithoutDetachingEntities();
+        String newsList = AppService.getGson().toJson(news);
+        GreenNews greenNews = new GreenNews(null, newsList, newsType);
+        greenNewsDao.insert(greenNews);
+
+    }
+
+    private static News getCacheNews(String newsType) {
+        News news=null;
+        GreenNewsDao greenNewsDao = AppService.getDBHelper().getDaoSession().getGreenNewsDao();
+        Query query = greenNewsDao.queryBuilder()
+                .where(GreenNewsDao.Properties.Type.eq(newsType))
+                .build();
+        // 查询结果以 List 返回
+        List<GreenNews> greenNewses = query.list();
+        if(greenNewses!=null&&greenNewses.size()>0) {
+            news = AppService.getGson().fromJson(greenNewses.get(0).getNewslist(), News.class);
+        }
+        return news;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/me/silencedut/nbaplus/rxmethod/RxStats.java
@@ -0,0 +1,162 @@
+package com.me.silencedut.nbaplus.rxmethod;
+
+import android.util.Log;
+
+import com.me.silencedut.greendao.GreenStat;
+import com.me.silencedut.greendao.GreenStatDao;
+import com.me.silencedut.nbaplus.app.AppService;
+import com.me.silencedut.nbaplus.data.Constant;
+import com.me.silencedut.nbaplus.event.StatEvent;
+import com.me.silencedut.nbaplus.model.Statistics;
+
+import java.util.List;
+
+import de.greenrobot.dao.query.DeleteQuery;
+import de.greenrobot.dao.query.Query;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by asan on 2015/12/15.
+ */
+public class RxStats {
+    public static Subscription getPerStat(String ...statKinds) {
+
+        Subscription subscription = Observable.from(statKinds)
+                .flatMap(new Func1<String, Observable<Statistics>>() {
+                    @Override
+                    public Observable<Statistics> call(String s) {
+                        return AppService.getNbaplus().getPerStats(s);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .doOnNext(new Action1<Statistics>() {
+                    @Override
+                    public void call(Statistics statistics) {
+                        cacheStat(statistics,statistics.getDailyStat().get(0).getStatkind());
+                    }
+                })
+                .subscribe(new Action1<Statistics>() {
+                    @Override
+                    public void call(Statistics statistics) {
+                        String[][] lables = getLables(statistics);
+                        float[][] ststValues=getStatValues(statistics);
+                        AppService.getBus().post(new StatEvent(statistics.getDailyStat().get(0).getStatkind(), lables,ststValues));
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        StatEvent statEvent = new StatEvent(throwable.toString(), null, null);
+                        statEvent.setEventResult(Constant.Result.FAIL);
+                        AppService.getBus().post(statEvent);
+
+                    }
+                });
+                return subscription;
+    }
+
+    public static Subscription initStat(final String statKind) {
+
+        Subscription subscription = Observable.create(new Observable.OnSubscribe<Statistics>() {
+                    @Override
+                    public void call(Subscriber<? super Statistics> subscriber) {
+                        Statistics statistics = getCacheStat(statKind);
+                        subscriber.onNext(statistics);
+                        subscriber.onCompleted();
+
+                    }
+                }).subscribeOn(Schedulers.io())
+                .subscribe(new Action1<Statistics>() {
+                    @Override
+                    public void call(Statistics statistics) {
+                        String[][] lables = getLables(statistics);
+                        float[][] ststValues=getStatValues(statistics);
+                        StatEvent statEvent = new StatEvent(statistics.getDailyStat().get(0).getStatkind(), lables, ststValues);
+                        statEvent.setGetNewsWay(Constant.GETNEWSWAY.INIT);
+                        AppService.getBus().post(statEvent);
+
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+
+                        StatEvent statEvent = new StatEvent(statKind, null, null);
+                        statEvent.setEventResult(Constant.Result.FAIL);
+                        statEvent.setGetNewsWay(Constant.GETNEWSWAY.INIT);
+                        AppService.getBus().post(statEvent);
+                    }
+                });
+        return subscription;
+    }
+
+    private static String[][] getLables(Statistics statistics) {
+        String[][] players=new String[2][5];
+        for(int index=0;index<5;index++) {
+            players[0][index]=parseLable(statistics.getDailyStat().get(index));
+            players[1][index]=parseLable(statistics.getEverageStat().get(index));
+        }
+        return players;
+    }
+
+    private static String parseLable(Statistics.StatEntity statEntity) {
+        StringBuilder lable =new StringBuilder();
+        String playerName=statEntity.getName();
+        for(int index=0;index<playerName.length()-3;index+=4) {
+            lable.append(playerName.substring(index,index+4));
+            lable.append("\n");
+        }
+        if(playerName.length()%4!=0) {
+            lable.append(playerName.substring(4 * (playerName.length() / 4), playerName.length()));
+            lable.append("\n");
+        }
+        lable.append("(");
+        lable.append(statEntity.getTeam());
+        lable.append(")");
+        return lable.toString();
+
+    }
+
+    private static float[][] getStatValues(Statistics statistics) {
+        float[][] statValues=new float[2][5];
+        for(int index=0;index<5;index++) {
+            statValues[0][index]=Float.parseFloat(statistics.getDailyStat().get(index).getStatdata());
+            statValues[1][index]=Float.parseFloat(statistics.getEverageStat().get(index).getStatdata());
+        }
+        return statValues;
+    }
+
+    public static void cacheStat(final Statistics stat,final String statKind) {
+
+        GreenStatDao greenStatDao = AppService.getDBHelper().getDaoSession().getGreenStatDao();
+        DeleteQuery deleteQuery = greenStatDao.queryBuilder()
+                .where(GreenStatDao.Properties.Statentity.eq(statKind))
+                .buildDelete();
+        deleteQuery.executeDeleteWithoutDetachingEntities();
+        String statEntity = AppService.getGson().toJson(stat);
+        GreenStat greenStat = new GreenStat(null, statEntity, statKind);
+
+        greenStatDao.insert(greenStat);
+        Log.d("StatEvent", greenStat.getStatkind() + "getStatkind");
+
+    }
+
+    private static Statistics getCacheStat(String statKind) {
+        Statistics statistics=null;
+        GreenStatDao greenStatDao = AppService.getDBHelper().getDaoSession().getGreenStatDao();
+        Query query = greenStatDao.queryBuilder()
+                .where(GreenStatDao.Properties.Statkind.eq(statKind))
+                .build();
+        // 查询结果以 List 返回
+        List<GreenStat> greenStats = query.list();
+        if(greenStats!=null&&greenStats.size()>0) {
+            statistics = AppService.getGson().fromJson(greenStats.get(0).getStatentity(), Statistics.class);
+        }
+        return statistics;
+    }
+
+
+}
