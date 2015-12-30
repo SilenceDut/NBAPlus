9255988d514086e6ec6a0565b0783b908d3226f1
SilenceDut <silencedutchman@foxmail.com>
2015-12-30 21:24:55 +0800

    replace erron team icon

--- app/src/main/java/com/me/silencedut/nbaplus/data/Constant.java
+++ app/src/main/java/com/me/silencedut/nbaplus/data/Constant.java
@@ -23,7 +23,7 @@ public class Constant {
             ,"灰熊","火箭","爵士","太阳","掘金","森林狼","国王","开拓者","鹈鹕","湖人"};
     public final static int[] TEAM_ICONS={R.mipmap.cleveland,R.mipmap.toronto,R.mipmap.atlanta,R.mipmap.indiana
             ,R.mipmap.miami,R.mipmap.detroit,R.mipmap.chicago,R.mipmap.orlando,R.mipmap.charlotte,R.mipmap.boston
-            ,R.mipmap.newyork,R.mipmap.houston,R.mipmap.milwaukee,R.mipmap.brooklyn,R.mipmap.phila,R.mipmap.goldenstate
+            ,R.mipmap.newyork,R.mipmap.washington,R.mipmap.milwaukee,R.mipmap.brooklyn,R.mipmap.phila,R.mipmap.goldenstate
             ,R.mipmap.sanantonio,R.mipmap.okc,R.mipmap.laclippers,R.mipmap.dallas,R.mipmap.memphis,R.mipmap.houston
             ,R.mipmap.utah,R.mipmap.phoenix,R.mipmap.denver,R.mipmap.minnesota,R.mipmap.sacramento,R.mipmap.portland
             ,R.mipmap.neworleans,R.mipmap.lalakers};
