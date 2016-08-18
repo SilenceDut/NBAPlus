aae93c620ae78d784ff43d6faf15b00c716ade25
SilenceDut <silencedutchman@foxmail.com>
2016-08-18 15:05:08 +0800

    add multidex

--- app/src/main/java/com/me/silencedut/nbaplus/app/App.java
+++ app/src/main/java/com/me/silencedut/nbaplus/app/App.java
@@ -2,6 +2,7 @@ package com.me.silencedut.nbaplus.app;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 //import com.squareup.leakcanary.LeakCanary;
 //import com.squareup.leakcanary.RefWatcher;
@@ -13,6 +14,13 @@ import im.fir.sdk.FIR;
  */
 public class App extends Application {
     private static Application sInstance;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
